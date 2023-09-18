0af29f186b89eb0d130cd858e2aaf7cc8152b321
hugesilver <xor3160@naver.com>
2023-09-18 14:03:28 +0900

    Initial Commit

---
+++ DallETest/DallETestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DallETestApp.swift
+//  DallETest
+//
+//  Created by 김태은 on 2023/09/18.
+//
+
+import SwiftUI
+
+@main
+struct DallETestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
